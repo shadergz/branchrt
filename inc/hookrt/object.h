379928325e63e26adff10e792a89a0cdbdec2c5d
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-10-09 16:08:04 -0400

    [v0.0] - Removing unnecessary "Object" section code

--- inc/hookrt/object.h
+++ inc/hookrt/object.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <memory>
-
 #include <hookrt/info.h>
-
 /*
 constexpr const char* LIB_PREFIX = "lib";
 constexpr const char* LIB_SUFFIX = ".so";
@@ -11,24 +8,10 @@ constexpr const char* LIB_SUFFIX = ".so";
 
 namespace hookrt::object {
 
-    class Native_Object {
-    protected:
-        Native_Object() = default;
-
-        Native_Object(const char* objNativeName) {
-            /* TODO: If objNativeName haven't a library prefix then add now,
-             * e.g: objiativeName = "module"; and after this operations below: libmodule.so */
-            nativeObjInfo = std::make_unique<info::Native_Info>(objNativeName);
-        }
-
-        Native_Object(info::Native_Info& info) {
-            nativeObjInfo = std::make_unique<info::Native_Info>(info);
-        }
-        auto getLibraryName() {
-            return nativeObjInfo->getObjectName();
-        }
-
-        std::unique_ptr<info::Native_Info> nativeObjInfo;
-    };
+struct Native_Object : public hookrt::info::Native_Info {
+    public:
+    Native_Object() {}
+    ~Native_Object() {}
+};
 };
 
